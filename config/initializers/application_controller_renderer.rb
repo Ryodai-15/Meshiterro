41b39fadad711641758116d00f42a3eb38dfb199
Ryodai-15 <gon.4629.1208@gmail.com>
2021-06-29 08:11:54 +0000

    [fix]gazoutoukou

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '566ad853e889ff6bddc2e740665d5b4065b3871cbf248b4581c6862950c09651d0da72f0e1fe8773684ac3c66dd4c2918920444f39dcf9c35053829fc9ff763a'
